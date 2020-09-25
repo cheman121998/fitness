7b64e03144072be009034ecec4e87713f1d845aa
cheman121998 <chethiminhman121998@gmail.com>
2020-09-25 14:16:57 +0700

    Updates

---
+++ precache-manifest.75f08e094f50d523dc04c5620432e30c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a71a784337fa1683ea38754855a4ed35",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1c9db678272135ac65d3",
+    "url": "/portfolio/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "97e63636bc8fb388b7b4",
+    "url": "/portfolio/static/js/2.ca42dc3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.ca42dc3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c9db678272135ac65d3",
+    "url": "/portfolio/static/js/main.f7ab99ce.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
